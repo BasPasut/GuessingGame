727694ed0f7e3e09568f31eaa48b77ad20246b68
pasut kittiprapas <pasut kittiprapas>
2017-01-20 18:26:22 +0700

    Master Version

--- src/GuessingGame/GuessingGame.java
+++ src/GuessingGame/GuessingGame.java
@@ -2,8 +2,9 @@ package GuessingGame;
 
 import java.util.Random;
 
-/** 
+/**
  * Class for GuessingGame.
+ * 
  * @author Pasut Kittiprapas.
  */
 public class GuessingGame {
@@ -12,31 +13,42 @@ public class GuessingGame {
 	private int secret;
 	private String hint;
 	public int count;
-	
+
 	/**
 	 * Initialize a new game.
-	 * @param upperbound is the max value for the secret number (>1).
+	 * 
+	 * @param upperbound
+	 *            is the max value for the secret number (>1).
 	 */
 	public GuessingGame(int upperBound) {
 		this.upperBound = upperBound;
 		this.secret = getRandomNumber(upperBound);
-		this.hint = "I'm thinking of a number between 1 and "+ upperBound;
+		this.hint = "I'm thinking of a number between 1 and " + upperBound;
 		this.count = 0;
 	}
-	public int getSecretNumber(){
+
+	/**
+	 * Get the secret number from the random.
+	 * @return secret number from the random.
+	 */
+	public int getSecretNumber() {
 		return this.secret;
 	}
+
 	/**
 	 * count the number of guesses
+	 * 
 	 * @return the number of guesses
 	 */
-	public int getCount(){
+	public int getCount() {
 		return this.count;
 	}
-	
-	/** 
+
+	/**
 	 * Create a random number between 1 and limit.
-	 * @param limit is the upper limit for random number
+	 * 
+	 * @param limit
+	 *            is the upper limit for random number
 	 * @return a random number between 1 and limit (inclusive)
 	 */
 	public int getRandomNumber(int limit) {
@@ -44,7 +56,12 @@ public class GuessingGame {
 		Random rand = new Random(seed);
 		return rand.nextInt(limit) + 1;
 	}
-
+	
+	/**
+	 * Guess the number and give it true or false.
+	 * @param number
+	 * @return true or false from guessing.
+	 */
 	public boolean guess(int number) {
 		this.count++;
 		if (number == this.secret) {
@@ -53,14 +70,15 @@ public class GuessingGame {
 		} else if (number > this.secret) {
 			this.hint = "Sorry, your guess is too large.";
 			return false;
-		} else{
+		} else {
 			this.hint = "Sorry, your guess is too small.";
 			return false;
 		}
 	}
-	
-	/** 
+
+	/**
 	 * Return a hint based on the most recent guess.
+	 * 
 	 * @return hint based on most recent guess
 	 */
 	public String getHint() {
